f4e141f558a0129b5890774833162d4ee9850fb3
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-08-08 22:02:55 -0300

    chore: add i18n

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
   // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "en",
-    locales: ["en"],
+    locales: ["en", "es", "fr", "ja", "ko", "pt-br", "ru", "zh-Hans"],
   },
 
   presets: [
@@ -75,6 +75,10 @@ const config = {
             label: "Install",
             position: "right",
             href: "https://github.com/Kindelia/Kind2#installation",
+          },
+          {
+            type: 'localeDropdown',
+            position: 'right',
           },
           {
             href: "https://github.com/kindela/kind2",
